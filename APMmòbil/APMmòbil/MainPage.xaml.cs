22b69d668c180aff654b483753c979b9724bea22
ladria <adria.ameijide@gmail.com>
2015-11-13 00:53:08 +0100

    2a versió
    Amb àudios (13 d'abril de 2012)

--- APMmòbil/APMmòbil/MainPage.xaml.cs
+++ APMmòbil/APMmòbil/MainPage.xaml.cs
@@ -21,6 +21,15 @@ namespace APMmòbil
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private SoundEffect bienSE;
+        private SoundEffect lioSE;
+        private SoundEffect carterasSE;
+        private SoundEffect follonSE;
+        private SoundEffect poconSE;
+        private SoundEffect vistSE;
+
+
+
         // Constructor
         public MainPage()
         {
@@ -29,50 +38,96 @@ namespace APMmòbil
 
         private void bien_Checked(object sender, RoutedEventArgs e)
         {
-            StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("/APMmòbil;component/Sound/bien.wav", UriKind.Relative));
-            SoundEffect se = SoundEffect.FromStream(streamInfo.Stream);
-            SoundEffectInstance bien = se.CreateInstance();
-            bien.Play();
+
+            try
+            {
+                StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("Sounds/bien.wav", UriKind.Relative));
+                bienSE = SoundEffect.FromStream(streamInfo.Stream);
+                SoundEffectInstance bien = bienSE.CreateInstance();
+                bien.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Can't play, Sound is null.");
+            }
+
         }
 
         private void lio_Checked(object sender, RoutedEventArgs e)
         {
-            StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("/APMmòbil;component/Sound/lio.wav", UriKind.Relative));
-            SoundEffect se = SoundEffect.FromStream(streamInfo.Stream);
-            SoundEffectInstance lio = se.CreateInstance();
-            lio.Play();
+
+            try
+            {
+                StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("Sounds/lio.wav", UriKind.Relative));
+                lioSE = SoundEffect.FromStream(streamInfo.Stream);
+                SoundEffectInstance lio = lioSE.CreateInstance();
+                lio.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Can't play, Sound is null.");
+            }
         }
 
         private void carteras_Checked(object sender, RoutedEventArgs e)
         {
-            StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("/APMmòbil;component/Sound/carteras.wav", UriKind.Relative));
-            SoundEffect se = SoundEffect.FromStream(streamInfo.Stream);
-            SoundEffectInstance carteras = se.CreateInstance();
-            carteras.Play();
+            
+            try
+            {
+                StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("Sounds/carteras.wav", UriKind.Relative));
+                carterasSE = SoundEffect.FromStream(streamInfo.Stream);
+                SoundEffectInstance carteras = carterasSE.CreateInstance();
+                carteras.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Can't play, Sound is null.");
+            }
         }
 
         private void follon_Checked(object sender, RoutedEventArgs e)
         {
-            StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("/APMmòbil;component/Sound/follon.wav", UriKind.Relative));
-            SoundEffect se = SoundEffect.FromStream(streamInfo.Stream);
-            SoundEffectInstance follon = se.CreateInstance();
-            follon.Play();
+            try
+            {
+                StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("Sounds/follon.wav", UriKind.Relative));
+                follonSE = SoundEffect.FromStream(streamInfo.Stream);
+                SoundEffectInstance follon = follonSE.CreateInstance();
+                follon.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Can't play, Sound is null.");
+            }
         }
 
         private void pocon_Checked(object sender, RoutedEventArgs e)
         {
-            StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("/APMmòbil;component/Sound/pocon.wav", UriKind.Relative));
-            SoundEffect se = SoundEffect.FromStream(streamInfo.Stream);
-            SoundEffectInstance pocon = se.CreateInstance();
-            pocon.Play();
+            try
+            {
+                StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("Sounds/pocon.wav", UriKind.Relative));
+                poconSE = SoundEffect.FromStream(streamInfo.Stream);
+                SoundEffectInstance pocon = poconSE.CreateInstance();
+                pocon.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Can't play, Sound is null.");
+            }
         }
 
         private void vist_Checked(object sender, RoutedEventArgs e)
         {
-            StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("/APMmòbil;component/Sound/vist.wav", UriKind.Relative));
-            SoundEffect se = SoundEffect.FromStream(streamInfo.Stream);
-            SoundEffectInstance vist = se.CreateInstance();
-            vist.Play();
+            try
+            {
+                StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("Sounds/vist.wav", UriKind.Relative));
+                vistSE = SoundEffect.FromStream(streamInfo.Stream);
+                SoundEffectInstance vist = vistSE.CreateInstance();
+                vist.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Can't play, Sound is null.");
+            }
         }
     }
 }
